ff486f77725c117af9f28ce9302bd54ffe6466db
Gautam696969 <palgautam69@gmail.com>
2025-07-10 12:32:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function App() {
+    console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <div>
       <h1>This is my BLOg</h1>
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     status: false,
-    userData: null      
+    userData: null
 }
 const authSlice = createSlice( {
     name: 'auth',
@@ -12,12 +12,13 @@ const authSlice = createSlice( {
             state.status = true;
             state.userData = action.payload.userData;
         },
-        logout: (state) =>{
+        logout: ( state ) => {
             state.status = false;
             state.userData = null;
-        }   
+        },
+        
     }
 } );
-export const {login , logout}  = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
